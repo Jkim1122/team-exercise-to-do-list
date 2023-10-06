17a392860402c96bb6d69807e1bbf32fe03841fe
Jkim1122 <jkimz1122@gmail.com>
2023-10-06 16:26:14 -0500

    following along

--- test_todo_manager.py
+++ test_todo_manager.py
@@ -1,4 +1,9 @@
-from todolist import practice
+# from todolist import practice
 
-def test_helloworldfunction():
-    assert practice(1, 2)==3
+# def test_helloworldfunction():
+#     assert practice(1, 2)==3
+
+from todolist import user_input
+
+def test_user_input():
+    assert type(user_input("What's up?")) ==str
