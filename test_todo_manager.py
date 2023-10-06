f595aae8cfca4557c69cdd21fa9711472e4be6af
Jkim1122 <jkimz1122@gmail.com>
2023-10-06 17:02:07 -0500

    got somewhere..

--- test_todo_manager.py
+++ test_todo_manager.py
@@ -4,6 +4,10 @@
 #     assert practice(1, 2)==3
 
 from todolist import user_input
+from todolist import add_answer
 
 def test_user_input():
     assert type(user_input("What's up?")) ==str
+
+def test_add_answer():
+    assert add_answer("Walk the dog") == ["Walk the dog"]
--- todolist.py
+++ todolist.py
@@ -2,8 +2,18 @@
 #     answer = num_one + num_two
 #     return answer
 
-def user_input(question):
-    answer = input(question)
-    return(answer)
+my_list = []
+answer = ""
 
-user_input("What's up?")
+while (True):
+    def user_input(question):
+        answer = input(question)
+        return(answer)
+
+    # user_input("What's is your task?")
+
+    def add_answer(task):
+        my_list.append(task)
+        print(my_list)
+
+    add_answer(user_input("What's is your task?"))
